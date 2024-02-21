496be843b6a8e5b558adab9ed8c9db8fc2373bbf
ukir-rrrr <aorikuaoriku@icloud.com>
2024-02-21 22:59:40 +0900

    Add new feature

---
+++ app.js
@@ -0,0 +1,19 @@
+var circle = document.getElementById("circle");
+      var upBtn = document.getElementById("upBtn");
+      var downBtn = document.getElementById("downBtn");
+
+      var rotateValue = circle.style.transform;
+      var rotateSum;
+
+      upBtn.onclick = function () {
+        /* 反時計回りに90°回転させる */
+        rotateSum = rotateValue + "rotate(-90deg)";
+        circle.style.transform = rotateSum;
+        rotateValue = rotateSum;
+      };
+      downBtn.onclick = function () {
+        /* 時計回りに90°回転させる */
+        rotateSum = rotateValue + "rotate(90deg)";
+        circle.style.transform = rotateSum;
+        rotateValue = rotateSum;
+      };
